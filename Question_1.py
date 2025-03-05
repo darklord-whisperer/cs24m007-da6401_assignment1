a0648c2aaa118e1e0ccabbfa3788907a981e7c39
darklord-whisperer <cs24m007@smail.iitm.ac.in>
2025-03-05 10:38:51 -0800

    Update Question_1.py
    Adding remaining functionalities left for visulaization and logging purposes into wandb along with main function

--- Question_1.py
+++ Question_1.py
@@ -10,3 +10,42 @@ class FashionMNISTVisualizer:
 
     def load_data(self):
         (self.train_images, self.train_labels), (_, _) = fashion_mnist.load_data()
+        
+    def plot_sample_images(self):
+        plt.figure(figsize=(12, 8))
+
+        # Iterate through each class to plot a sample image for each
+        for class_idx in range(len(self.class_labels)):
+            # Find the first occurrence of an image with the current label
+            first_index = np.where(self.train_labels == class_idx)[0][0]
+
+            sample_image = self.train_images[first_index]
+            label = self.class_labels[self.train_labels[first_index]]
+
+            plt.subplot(2, 5, class_idx + 1)
+            plt.imshow(sample_image, cmap=plt.cm.binary)
+            plt.title(label)
+            plt.axis('off')
+
+        plt.show()
+
+    def log_images_to_wandb(self):
+        wandb.init(project="Alik_Final_DA6401_1_DeepLearing_Assignment1", name="Quuestion_1_Fashion_MNIST_Grid_Visualization")
+        wandb.log({"Fashion_MNIST_Sample_Images": wandb.Image(plt)})
+        
+        wandb.finish()
+
+
+def main():
+    # Initializing visualizer class
+    visualizer = FashionMNISTVisualizer()
+    
+    # Loading dataset
+    visualizer.load_data()
+
+    visualizer.plot_sample_images()
+
+    visualizer.log_images_to_wandb()
+
+if __name__ == "__main__":
+    main()
